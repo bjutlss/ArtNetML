6f42705d5953d887a975e175d61e0dd6f39ceb4b
Miriam <miriam.jost@study.hs-duesseldorf.de>
2022-05-19 14:01:59 +0200

    changes to port number

--- ffb6d/train_custom.sh
+++ ffb6d/train_custom.sh
@@ -2,4 +2,4 @@
 
 n_gpu=2
 cls='vase'
-python -m torch.distributed.launch --master_port 44440 --nproc_per_node=$n_gpu train_custom.py --gpus=$n_gpu --cls=$cls
+python -m torch.distributed.launch --master_port 33330 --nproc_per_node=$n_gpu train_custom.py --gpus=$n_gpu --cls=$cls
