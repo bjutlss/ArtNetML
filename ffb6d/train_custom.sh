517931a794c972e4a6e15e205dced535fbc6e121
Miriam <miriam.jost@study.hs-duesseldorf.de>
2022-05-19 20:32:33 +0200

    added masterport

--- ffb6d/train_custom.sh
+++ ffb6d/train_custom.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 n_gpu=8
 cls='vase'
-torchrun --nproc_per_node=$n_gpu train_custom.py --master_port=44440 --gpus=$n_gpu --cls=$cls
+torchrun --nproc_per_node=$n_gpu train_custom.py --master_port=55550 --gpus=$n_gpu --cls=$cls
