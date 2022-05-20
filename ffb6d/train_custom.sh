a815587d9428ee001016a6d8a530b271261788d5
Miriam <miriam.jost@study.hs-duesseldorf.de>
2022-05-20 03:26:58 +0200

    try with 1 gpu

--- ffb6d/train_custom.sh
+++ ffb6d/train_custom.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 n_gpu=1
 cls='vase'
-python -m torch.distributed.launch --nproc_per_node=1 train_lm.py --gpu "0" --gpus 1
+python -m torch.distributed.launch --nproc_per_node=1 train_custom.py --gpu "0" --gpus 1
